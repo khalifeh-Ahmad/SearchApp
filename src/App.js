29900c05999bccd8cd990123fd9a8743f17b6986
khalifeh Ahmad <khalifeh92ahmed@hotmail.com>
2023-10-03 12:24:46 -0800

    Config

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import axios from "axios";
 
 function App() {
+  const [term, setTerm] = useState("react");
+  const [list, setList] = useState([]);
+  useEffect(() => {
+    const searchAPI = async () => {
+      const res = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          format: "json",
+          origin: "*",
+          list: "search",
+          srsearch: term,
+        },
+      });
+      setList(res.data.query.search);
+    };
+    if (term) {
+      searchAPI();
+    }
+  }, [term]);
+
+  const showRes = list.map((dt, idx) => (
+    <tr key={dt.pageid} style={{ border: "1px solid black" }}>
+      <td>{idx}</td>
+      <td>{dt.title}</td>
+      <td>
+        <span dangerouslySetInnerHTML={{ __html: dt.snippet }} />
+      </td>
+    </tr>
+  ));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row className="mt-4">
+        <Col>
+          {" "}
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Control
+                type="text"
+                placeholder="Search Input"
+                onChange={(e) => setTerm(e.target.value)}
+                value={term}
+              />
+            </Form.Group>
+          </Form>
+        </Col>
+      </Row>
+      <Row className="mt-3">
+        <Col>
+          <table style={{ border: "1px solid gray" }}>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Title</th>
+                <th>Description</th>
+              </tr>
+            </thead>
+            <tbody>{showRes}</tbody>
+          </table>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
